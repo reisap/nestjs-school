b140ed1768a215c25e55ae87cedc6ec674e306f6
Reisa Prasaptaraya <r.prasaptaraya@gmail.com>
2024-05-18 19:44:49 +0700

    [etc] fix issue config csurf

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,7 +9,6 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './users.repository';
 import { User } from './entities/user.entity';
-import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 import * as bcrypt from 'bcryptjs';
 import { randomString } from '@app/common';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -18,139 +17,123 @@ import { ChangePasswordUser } from './dto/change-password-user.dto';
 @Injectable()
 export class UsersService {
   private readonly logger = new Logger(UsersService.name);
+
   constructor(private readonly userRepository: UsersRepository) {}
 
   async changeUserPassword(changeUserPassword: ChangePasswordUser) {
-    //1. check email ada di database atau tidak
-    //2. jika ada check password yang dulu dimasukan benar atau tidak
-    //3. update password dengan yang baru
     try {
-      //1
-      const result = await this.userRepository.findOneParams({
+      const user = await this.userRepository.findOneParams({
         email: changeUserPassword.email,
       });
-
-      if (!result || result == undefined || result == null) {
+      if (!user) {
         throw new BadRequestException('email not found !!');
       }
 
-      //2
-      const match = await bcrypt.compare(
+      const isMatch = await bcrypt.compare(
         changeUserPassword.password,
-        result.password,
+        user.password,
       );
-      if (!match) {
+      if (!isMatch) {
         throw new BadRequestException('password not match !!');
       }
-      //3
-      const user = await this.userRepository.update(result.id, {
-        password: changeUserPassword.new_password,
-      });
 
-      return user;
+      user.password = await bcrypt.hash(changeUserPassword.new_password, 12);
+      return await this.userRepository.update(user.id, user);
     } catch (e) {
-      throw new BadRequestException(e);
+      this.logger.error('Error changing user password', e);
+      throw new BadRequestException(e.message);
     }
   }
 
   async verifyUserByEmailToken(token: string) {
     try {
-      //search user by token generate
       const user = await this.userRepository.findOneParams({
         activationToken: token,
       });
-      if (!user || user == null || user == undefined) {
+      if (!user) {
         throw new BadRequestException('token activation expired !');
       }
-      //update status user active
-      const result = await this.userRepository.update(user.id, {
-        inactive: false,
-        activationToken: null,
-      });
 
-      return result;
+      user.inactive = false;
+      user.activationToken = null;
+      return await this.userRepository.update(user.id, user);
     } catch (e) {
-      throw new BadRequestException(e);
+      this.logger.error('Error verifying user by email token', e);
+      throw new BadRequestException(e.message);
     }
   }
 
   async login(loginUser: LoginUserDto) {
-    const user = await this.userRepository.findOneParams({
-      email: loginUser.email,
-    });
-    if (!user) {
-      throw new NotFoundException('email not found');
-    }
-    const match = await bcrypt.compare(loginUser.password, user.password);
-    if (!match) {
-      throw new BadRequestException('password not match');
-    }
-    return user;
-  }
-  async create(createUserDto: CreateUserDto): Promise<User | any> {
     try {
-      const result = await this.userRepository.save({
+      const user = await this.userRepository.findOneParams({
+        email: loginUser.email,
+      });
+      if (!user) {
+        throw new NotFoundException('email not found');
+      }
+
+      const isMatch = await bcrypt.compare(loginUser.password, user.password);
+      if (!isMatch) {
+        throw new BadRequestException('password not match');
+      }
+
+      return user;
+    } catch (e) {
+      this.logger.error('Error logging in', e);
+      throw new BadRequestException(e.message);
+    }
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const newUser = {
         ...createUserDto,
         password: await bcrypt.hash(createUserDto.password, 12),
         activationToken: randomString(16),
-      });
-      return result;
+      };
+      return await this.userRepository.save(newUser);
     } catch (e) {
-      throw new UnprocessableEntityException(e);
+      this.logger.error('Error creating user', e);
+      throw new UnprocessableEntityException(e.message);
     }
   }
 
   async findAll(page: number, limit: number): Promise<User[]> {
     try {
-      const options = {
-        skip: page,
-        take: limit,
-      };
-      return await this.userRepository.findAll(options);
+      return await this.userRepository.findAll({ skip: page, take: limit });
     } catch (e) {
-      this.logger.error('error findAll');
-      throw new ExceptionsHandler(e);
+      this.logger.error('Error finding all users', e);
+      throw new BadRequestException(e.message);
     }
   }
 
   async findOne(id: number): Promise<User> {
     try {
-      const result = await this.userRepository.findOneById(id);
-      return result;
+      return await this.userRepository.findOneById(id);
     } catch (e) {
-      throw new BadRequestException(e);
+      this.logger.error('Error finding user by ID', e);
+      throw new BadRequestException(e.message);
     }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
     try {
-      const result = await this.userRepository.update(id, {
+      return await this.userRepository.update(id, {
         ...updateUserDto,
         update_date: new Date().toISOString(),
       });
-      return result;
     } catch (e) {
-      throw new BadRequestException(e);
+      this.logger.error('Error updating user', e);
+      throw new BadRequestException(e.message);
     }
   }
 
   async remove(id: number) {
     try {
-      const result = await this.userRepository.delete(id);
-      return result;
+      return await this.userRepository.delete(id);
     } catch (e) {
-      throw new BadRequestException(e);
+      this.logger.error('Error removing user', e);
+      throw new BadRequestException(e.message);
     }
   }
 }
-
-/*
-//ini contoh jika ingin menggunakan abstract services
-//alasan kenapa tidak menggunakan cara ini agar memisahkan proses bisnis dengan proses presenter/controller, jadi jangan ada logic bisnis di sebuah controller
-@Injectable()
-export class UsersService extends AbstractService {
-  constructor(private readonly userRepository: Repository<User>) {
-    super(userRepository);
-  }
-}
-*/
